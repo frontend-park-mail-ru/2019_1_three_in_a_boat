4cd45030e6231f676c1ed2b4cc70f1744032a161
Kotyarich <ndkotov@gmail.com>
2019-03-03 17:24:13 +0300

    Login page in SPA almost ready

--- static/main.js
+++ static/main.js
@@ -179,10 +179,154 @@ function createAuthors() {
       createMenuLink();
 }
 
+function createLoginPage() {
+    createHeader();
+
+    const template = [{
+        block: 'login-popup',
+        content: [
+            {
+                block: 'title',
+                wrappedInside: 'login-popup',
+                wrappedAs: 'title',
+                content: 'Войти'
+            },
+            {
+                block: 'icon',
+                wrappedInside: 'login-popup',
+                wrappedAs: 'close-popup',
+                mods: {
+                    size: 'large',
+                    color: 'black',
+                    btn: true,
+                    type: 'cross'
+                }
+            },
+            {
+                block: 'login-form',
+                content: [
+                    {
+                        block: 'form-group',
+                        mods: {size: 'inline'},
+                        content: [
+                            {
+                                block: 'form-group',
+                                mods: {size: 'large'},
+                                content: {
+                                    block: 'input',
+                                    wrappedInside: 'login-form',
+                                    wrappedAs: 'input',
+                                    attrs: {placeholder: 'Телефон или электронная почта'}
+                                }                        
+                            },
+                            {
+                                block: 'input',
+                                mods: {with: 'icon'},
+                                wrappedInside: 'login-form',
+                                wrappedAs: 'input',
+                                content: [
+                                    {
+                                        elem: 'field',                                        
+                                        attrs: {type: 'password', placeholder: 'Пароль'}
+                                    },
+                                    {
+                                        block: 'icon',
+                                        wrappedInside: 'input',
+                                        wrappedAs: 'icon',
+                                        mods: {
+                                            color: 'black',
+                                            btn: true,
+                                            type: 'eye'
+                                        }   
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    {
+                        block: 'form-group',
+                        mods: {align: 'space-between', size: 'large'},
+                        content: [
+                            {
+                                content: [
+                                    {
+                                        block: 'checkbox',
+                                        wrappedInside: 'login-form',
+                                        wrappedAs: 'remember-me-checkbox',
+                                        mods: {size: 'small'},
+                                        attrs: {type: 'checkbox', id: 'login-form__remember-me-checkbox'}                                
+                                    },
+                                    {
+                                        tag: 'label',
+                                        block: 'login-form',
+                                        elem: 'remember-me-label',
+                                        attrs: {for: 'login-form__remember-me-checkbox'},
+                                        content: 'Запомнить пароль'                                        
+                                    }
+                                ]
+                            },
+                            {
+                                content: {
+                                    tag: 'a',
+                                    block: 'login-form',
+                                    elem: 'forgot-password-link',
+                                    href: '#',
+                                    content: 'Забыли пароль?'
+                                }
+                            }
+                        ]
+                    },
+                    {
+                        block: 'form-group',
+                        mods: {align: 'center'},
+                        wrappedInside: 'login-form',
+                        wrappedAs: 'form-group',
+                        content: [
+                            {
+                                block: 'btn',
+                                wrappedInside: 'login-form',
+                                wrappedAs: 'sumbit-btn',
+                                attrs: {type: 'submit'},
+                                content: [{
+                                    elem: 'inner',
+                                    content: {
+                                        elem: 'text',
+                                        content: 'Войти'
+                                    }
+                                }]
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                block: 'hr',
+                wrappedInside: 'login-popup',
+                wrappedAs: 'hr',
+            },
+            {
+                elem: 'signup-link',
+                content: {
+                    tag: 'a',
+                    href: '#',
+                    content: 'Зарегистрироваться'
+                }
+            }            
+        ]
+    }];
+
+    document.getElementById('application').insertAdjacentHTML('beforeend',
+        bemhtml.apply(template)
+    );
+
+    createMenuLink();
+}
+
 createMenu();
 
 const pages = {
     menu: createMenu,
+    signIn: createLoginPage,
     authors: createAuthors,
 	leaders: createScoreBoard,
 };
