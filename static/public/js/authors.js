5ea5f915692a3680ecd19e3fdea602d0d8f1c38c
Kotyarich <ndkotov@gmail.com>
2019-03-05 17:33:42 +0300

    main.js divided on modules

---
+++ static/public/js/authors.js
@@ -0,0 +1,43 @@
+import createHeader from "./header.js";
+
+export default function createAuthors() {
+  createHeader();
+
+  const template = [
+    {
+      block: 'authors',
+      content: [
+        {
+          elem: 'header',
+          content: 'Проект разработали'
+        },
+        {
+          elem: 'items',
+          authors: [
+            {
+              name: 'Никита',
+              devInfo: 'фронт/бэк/фуллкек',
+              img: 'images/nikita.jpg',
+              description: '01234'
+            },
+            {
+              name: 'Арсен',
+              devInfo: 'фронт/бэк/фуллкек',
+              img: 'images/pepe.jpg',
+              description: '56789'
+            },
+            {
+              name: 'Андрей',
+              devInfo: 'фронт/бэк/фуллкек',
+              img: 'images/andrey.jpg',
+              description: 'Цифры закончились, Андрей, раньше надо было приходить'
+            }
+          ]
+        }
+      ]
+    }];
+
+  document.getElementById('application').insertAdjacentHTML('beforeend',
+    bemhtml.apply(template)
+  );
+}
