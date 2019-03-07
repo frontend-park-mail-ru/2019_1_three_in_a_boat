681ca5b4792b6bb0fcf2c33a7e0bd4abaa471fab
Andrey Prokopenko <prokopenko.rower@mail.ru>
2019-03-07 10:02:19 +0300

    changed template signature

--- static/blocks/signup-popup/template.js
+++ static/blocks/signup-popup/template.js
@@ -1,3 +1,3 @@
-block('signup-popup')({tag: 'form'});
+// block('signup-popup')({tag: 'form'});
 block('signup-popup').elem('hr')({tag: 'hr'});
 block('signup-popup').elem('explanation-text')({tag: 'span'});
