0d1ac16c0cb6c1a4cfbea5644dc854c5b39890ef
Kotyarich <ndkotov@gmail.com>
2019-04-04 22:32:55 +0300

    Remove extra methods and files

--- static/public/js/models/user-service.js
+++ static/public/js/models/user-service.js
@@ -80,46 +80,6 @@ export default class UserService extends Model {
   }
 
   /**
-   * Get data from input form on sign up page
-   * @param {event} event
-   * @return {Object} input form
-   */
-  static getFromSignUp(event) {
-    if (!validateForm(event.target)) {
-      return;
-    }
-
-    const form = document.getElementById('signup-form');
-
-    const name = form['signup-form_firstName'].value;
-    const lastName = form['signup-form_lastName'].value;
-    const email = form['signup-form_email'].value;
-    const userName = form['signup-form_username'].value;
-
-    const selectField = document.getElementsByTagName('select');
-    const day = selectField['signup-form_selectDay'].value;
-    const month = selectField['signup-form_selectMonth'].value;
-    const year = selectField['signup-form_selectYear'].value;
-    const date = `${day}-${month}-${year}`;
-
-    const password = form['signup-form_password'].value;
-
-    return {userName, password, name, lastName, email, date};
-  }
-
-  /**
-   * Get data from user input on login page
-   * @param {event} event
-   * @return {{password: *, name: *}}
-   */
-  static getFormLogin(event) {
-    const form = event.target;
-    const name = form.elements['email'].value;
-    const password = form.elements['password'].value;
-    return {name, password};
-  }
-
-  /**
    * Get list of users with pagination
    * @param {string} page
    * @return {Promise<any | never>}
